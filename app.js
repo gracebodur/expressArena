b2e7f4870a1da0ec13b09371d7522e1950f89dd1
Grace <mgracebodur@gmail.com>
2019-12-11 02:02:36 -0500

    Logging morgan

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
+app.use(morgan('dev'))
 
 //to create a get route we use the .get() method where PATH is 
 //the path on the server and HANDLER is a function to be executed 
