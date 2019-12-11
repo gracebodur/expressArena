9525883a3a4dbb6105d07b9cc5c3b6ae2cc96181
Grace <mgracebodur@gmail.com>
2019-12-11 01:36:23 -0500

    Simple HTTP request

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('starting with express')
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello Grace!')
+})
+
+app.listen(8000, () => {
+    console.log('Express server is listening on port 8000!')
+})
