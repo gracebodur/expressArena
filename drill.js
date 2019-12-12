a6b991a6e78df7ccabeff2ece73a8fbef7cd925b
Grace <mgracebodur@gmail.com>
2019-12-12 02:03:39 -0500

    Solved drill#1 and 2

--- drill.js
+++ drill.js
@@ -4,24 +4,57 @@ const morgan = require ('morgan')
 const drill = express()
 drill.use(morgan('dev'))
 
+//Drill 1
+
 drill.get('/sum', (req, res) => {
 
     const a = parseInt(req.query.a)
     const b = parseInt(req.query.b)
-    const c = a + b
+    
 
-    if(Number.isNaN(a) || Number.isNaN(b)) {
+    if(Number.isNaN(a) && Number.isNaN(b)) {
         return res.status(400).send('Please provide a number');
     }
-    if(!a) {
-        return res.status(400).send('Please provide a');
-    }
-    if(!b) {
-        return res.status(400).send('Please provide b');
+
+    // if(!a) {
+    //     return res.status(400).send('a is required');
+    // }
+
+    // if(!b) {
+    //     return res.status(400).send('b is required');
+    // }
+
+    const c = a + b
+    
+    res.send(`The sum of ${a} and ${b} is ${c}`)
+})
+
+
+//Drill 2
+
+drill.get('/cipher',(req, res) => {
+    const text = (req.query.text)
+    const shift = parseInt(req.query.shift)
+
+    if(!text) {
+        return res.status(400).send('text is required')
     }
 
-    const sum = `The sum of ${a} and ${b} is ${c}`;    
-    res.send(sum)
+    if(!shift) {
+        return res.status(400).send('shift is required')
+    }
+
+    if(Number.isNaN(shift)) {
+        return res.status(400).send('A number is required for shift')
+    }
+
+    const encrypt = text
+    .split('')
+    .map(letter => {
+        const code = letter.charCodeAt(0) + shift
+        return String.fromCharCode(code)
+    })
+    res.send(encrypt.join(''))
 })
 
 drill.listen(8000, () => {
