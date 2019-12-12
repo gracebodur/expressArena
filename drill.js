796c9f25ec36abda7812bca0e231910d3ccc0057
Grace <mgracebodur@gmail.com>
2019-12-12 18:38:46 -0500

    Retrying drill#3

--- drill.js
+++ drill.js
@@ -51,7 +51,7 @@ drill.get('/cipher',(req, res) => {
     }
 
     const encrypt = text.split('').map(letter => {
-        const code = letter.charCodeAt(0) + shift
+    const code = letter.charCodeAt(0) + shift
         return String.fromCharCode(code)
     })
     res.send(encrypt.join(''))
@@ -69,54 +69,9 @@ drill.get('/cipher',(req, res) => {
 //If all 6 numbers match respond with "Wow! Unbelievable! You could have won the mega millions!".
 
 drill.get('/lotto', (req, res) => {
-    const { numbers } = req.query; 
+    const { numbers } = req.query
 
-    if(!numbers) {
-    return res.status(200).send("numbers is required");
-    }
-
-    if(!Array.isArray(numbers)) {
-    return res.status(200).send("numbers must be an array");
-    }
-
-    const guesses = numbers.map(n => parseInt(n)).filter(n => !Number.isNaN(n) && (n >= 1 && n <= 20));
-    
-    if(guesses.length != 6) {
-    return res.status(400).send("numbers must contain 6 integers between 1 and 20");
-    }      
-    // 20 numbers to choose from
-    const stockNumbers = Array(20).fill(1).map((_, i) => i + 1);
-    //randomly choose 6
-    const winningNumbers = [];
-
-    for(let i = 0; i < 6; i++) {
-    const random = Math.floor(Math.random() * stockNumbers.length);
-    winningNumbers.push(stockNumbers[ran]);
-    stockNumbers.splice(ran, 1);
-    }
-
-    //compare the guesses to the winning number
-    let diff = winningNumbers.filter(n => !guesses.includes(n));
-
-    // construct a response
-    let responseText;
-    switch(diff.length){
-    case 0: 
-        responseText = 'Wow! Unbelievable! You could have won the mega millions!';
-        break;
-    case 1:   
-        responseText = 'Congratulations! You win $100!';
-        break;
-    case 2:
-        responseText = 'Congratulations, you win a free ticket!';
-        break;
-    default:
-        responseText = 'Sorry, you lose';  
-    }
-
-    res.send(responseText);
 })
-
 
 drill.listen(8000, () => {
     console.log('Express server is listening on port 8000!')
